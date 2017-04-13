586a46fb2a2f6bd297ac2382676e75612db6fe91
sagar <sagar@digitalprizm.net>
2017-04-13 15:13:38 +0530

    Updated with custom button to delivery request

--- delivery_management/delivery_management/doctype/delivery_request/delivery_request.js
+++ delivery_management/delivery_management/doctype/delivery_request/delivery_request.js
@@ -5,8 +5,91 @@ cur_frm.add_fetch('product_code', 'product_name', 'product_name');
 cur_frm.add_fetch('product_code', 'description', 'description');
 
 
-frappe.ui.form.on('Delivery Request', {
-	refresh: function(frm) {
+// frappe.ui.form.on('Delivery Request', {
+// 	refresh: function(frm) {
 
-	}
+// 	}
+// });
+
+
+frappe.ui.form.on('Delivery Request', {
+    refresh: function(frm) {
+    	var me = this;
+		cur_frm.add_custom_button(__('Make Delivery Order'), 
+			function(){
+				frappe.model.open_mapped_doc({
+					method: "delivery_management.delivery_management.doctype.delivery_request.delivery_request.make_delivery_order",
+					frm: cur_frm
+				})
+		}, __("Make"));
+  	}
+
+ //  	make_delivery_order: function() {
+
+	// },
+});
+
+// frappe.ui.form.on('Case', {
+// 	refresh: function(frm) {
+// 		if(frm.doc.docstatus == 1) {
+// 			cur_frm.add_custom_button(__("Create Invoice"),
+// 				function() {
+// 					frappe.model.open_mapped_doc({
+// 							method: "clinic_management.clinic_management.doctype.case.case.create_quote",
+// 							frm: cur_frm
+// 					})
+// 				})
+// 			}
+// 	},
+// 	onload: function(frm) {
+// 		if(frm.doc.__islocal == 1) {
+// 			frm.doc.company = "";
+// 		}
+// 	},
+	// company: function(frm) {
+	// 	if(!frm.doc.doctor) frappe.throw(__("Please select Doctor first"));
+	// },
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+frappe.ui.form.on("Delivery Request", "on_update", function(frm) {
+    frm.add_custom_button(__("Make Delivery Order"), function() {
+        
+
+        var subject = frm.doc.subject;
+        var event_type = frm.doc.event_type;
+
+        // do something with these values, like an ajax request 
+        // or call a server side frappe function using frappe.call
+        $.ajax({
+            url: "http://example.com/just-do-it",
+            data: {
+                "subject": subject,
+                "event_type": event_type
+            }
+
+            // read more about $.ajax syntax at http://api.jquery.com/jquery.ajax/
+
+        });
+    });
 });
--- delivery_management/delivery_management/doctype/delivery_request/delivery_request.py
+++ delivery_management/delivery_management/doctype/delivery_request/delivery_request.py
@@ -4,7 +4,61 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+
+import json
+from datetime import timedelta
+
+from frappe.utils import flt, time_diff_in_hours, get_datetime, getdate, today, cint, get_datetime_str
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class DeliveryRequest(Document):
-	pass
+	def validate(self):
+		pass
+
+
+# @frappe.whitelist()
+# def make_delivery_order():
+# 	return "hey there js function called me"
+	# def set_missing_values(source, target):
+		# doclist = get_mapped_doc("Case", source_name, {
+		# 	"Case": {
+		# 		"doctype": "Sales Invoice",
+		# 		"validation": {
+		# 			"docstatus": ["=", 1]
+		# 		},
+		# 	}
+		# }, target_doc, set_missing_values, ignore_permissions=False)
+
+
+@frappe.whitelist()
+def make_delivery_order(source_name, target_doc=None):
+	frappe.msgprint("helloooo")
+	def set_missing_values(source, target):
+		# do_name=frappe.db.get_value("Delivery Requset",{"company_name":source.doctor},"company_name")
+		# company_cost_center = frappe.db.get_value("Company",{"company_name":source.doctor},"cost_center")
+		# company_income_account = frappe.db.get_value("Company",{"company_name":source.doctor},"default_income_account")
+		# company_additional_info = frappe.db.get_value("Company",{"company_name":source.company},"additional_info")
+		# target.customer = source.customer
+		# target.company = source.company
+		# target.shipping_date = today()
+		# target.set('items', [])
+		# k = target.append('items', {})
+		# k.name = source_name
+		# k.product_code = source.product_code
+		# k.product_name = source.product_name
+		# k.description = source.description
+		# k.quantity = source.quantity
+		
+				
+
+	doclist = get_mapped_doc("Delivery Request", source_name, {
+			"Delivery Request": {
+				"doctype": "Delivery Order",
+				"validation": {
+					"docstatus": ["=", 0]
+				},
+			}
+		}, target_doc, set_missing_values, ignore_permissions=False)
+	return doclist
