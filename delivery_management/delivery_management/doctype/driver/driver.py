5a09aea9d763073e1363547d42833a2a432a2ce6
sagar <sagar@digitalprizm.net>
2017-04-24 11:50:53 +0530

    modified full name constraints in driver doctype

--- delivery_management/delivery_management/doctype/driver/driver.py
+++ delivery_management/delivery_management/doctype/driver/driver.py
@@ -5,7 +5,15 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+# from frappe import throw, _
 
 class Driver(Document):
 	def validate(self):
-		self.full_name = self.first_name + " " + self.last_name
+		self.set_full_name()
+
+	def set_full_name(self):
+		if not self.last_name:
+			self.full_name = self.first_name
+		else:
+			self.full_name = self.first_name + " " + self.last_name
+
