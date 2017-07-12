883ef1286140992bacaf961bb0331fff6ec3c624
prashante114 <prashant@digitalprizm.net>
2017-07-12 13:10:34 +0530

    updated api delivery shedule

--- delivery_management/public/js/customer_import_tool.js
+++ delivery_management/public/js/customer_import_tool.js
@@ -1,19 +1,19 @@
-frappe.pages['customer-import-tool'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Customer Import Tool',
-		single_column: true
-	});
-}
+// frappe.pages['customer-import-tool'].on_page_load = function(wrapper) {
+// 	var page = frappe.ui.make_app_page({
+// 		parent: wrapper,
+// 		title: 'Customer Import Tool',
+// 		single_column: true
+// 	});
+// }
 
 
-frappe.CustomDataImportTool = frappe.DataImportTool.extend({
-	init: function(parent) {
-		console.log("custom init")
-		this._super(parent)
-		this.onload()
-	},
-	onload: function(parent) {
-		console.log(" custom onload")
-	}
-})
+// frappe.CustomDataImportTool = frappe.DataImportTool.extend({
+// 	init: function(parent) {
+// 		console.log("custom init")
+// 		this._super(parent)
+// 		this.onload()
+// 	},
+// 	onload: function(parent) {
+// 		console.log(" custom onload")
+// 	}
+// })
