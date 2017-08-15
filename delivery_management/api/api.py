a79b58dadaf27dc131f2bc7bdcf621a455639420
sambhaji <kolate.sambhaji@gmail.com>
2017-08-15 20:39:52 +0530

    fixed send email

--- delivery_management/api/api.py
+++ delivery_management/api/api.py
@@ -286,8 +286,10 @@ def send_delivery_dispatch_alert(name):
 	# attachments = ds_doc.get_attachments()
 	recipients = ds_doc.email
 	
-	ds_doc.send_email(recipients, sender, subject, message, attachments=[frappe.attach_print("Delivery Schedule", name, file_name=name,print_format="Delivery Schedule")])
-
+	# ds_doc.send_email(recipients, sender, subject, message, attachments=[frappe.attach_print("Delivery Schedule", name, file_name=name,print_format="Delivery Schedule")])
+	frappe.sendmail(recipients=recipients, sender=sender, subject=subject,
+			message=message,  attachments=[frappe.attach_print("Delivery Schedule", name, file_name=name,print_format="Delivery Schedule")])
+	
 	ds_doc.save(ignore_permissions=True)
 
 	#send sms
