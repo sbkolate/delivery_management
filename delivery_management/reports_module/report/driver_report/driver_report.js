70f23967a2b06521f6fccc3f2538daba5d212fde
sambhaji <kolate.sambhaji@gmail.com>
2017-10-04 20:53:25 +0530

    added

---
+++ delivery_management/reports_module/report/driver_report/driver_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, DPI and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Driver Report"] = {
+	"filters": [
+
+	]
+}
