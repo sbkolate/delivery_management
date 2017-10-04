a84bb044fc09ba52587fb4a71f4e70e1adf19f23
prashante114 <prashant@digitalprizm.net>
2017-10-04 18:01:33 +0530

    script report delivery schedule

--- delivery_management/delivery_management/report/delivery_schedule_report/delivery_schedule_report.js
+++ delivery_management/delivery_management/report/delivery_schedule_report/delivery_schedule_report.js
@@ -5,11 +5,19 @@
 frappe.query_reports["Delivery Schedule Report"] = {
 	"filters": [
 		{
-			"fieldname":"date",
-			"label": __("Date"),
+			"fieldname":"from_date",
+			"label": __("From Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today(),
 			"reqd":1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd":1
+			
 		},
 
 	]
