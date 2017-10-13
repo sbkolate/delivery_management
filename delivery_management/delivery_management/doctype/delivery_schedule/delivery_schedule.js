b7d8ccdcb0f31069e35dae9642e3883a5eb39eea
sambhaji <kolate.sambhaji@gmail.com>
2017-10-13 13:54:57 +0530

    added

--- delivery_management/delivery_management/doctype/delivery_schedule/delivery_schedule.js
+++ delivery_management/delivery_management/doctype/delivery_schedule/delivery_schedule.js
@@ -5,6 +5,9 @@ cur_frm.add_fetch('driver', 'carrier', 'carrier');
 
 frappe.ui.form.on('Delivery Schedule', {
 	refresh: function(frm) {
+		if(frm.doc.status == "Delivered") {
+			cur_frm.set_df_property("delivery_note_no","read_only",1)
+		}
 		if(frm.doc.__islocal == 1) {
 			cur_frm.add_custom_button(__('<i class="fa fa-home" title="Back" style="margin-left: 10px;background-color: red;padding: 6px;margin: -10px;border-radius: 5px;color: white;"> Cancel</i>'),
 			function () { frappe.set_route("/"); }, 'fa fa-home btn-default', 'btn-danger')
@@ -31,6 +34,9 @@ frappe.ui.form.on('Delivery Schedule', {
 		}
 	},
 	onload: function(frm, cdt, cdn) {
+		if(cur_frm.doc.status == "Delivered") {
+			cur_frm.set_df_property("delivery_note_no","read_only",1)
+		}
 		cur_frm.set_query("carrier", function(){
 		return{
 				query: "delivery_management.delivery_management.doctype.carrier.carrier.get_carrier",
