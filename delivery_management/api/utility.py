6ba5b75fbe16ab8f34f47357f5e5f1bba6f03c02
prashante114 <prashant@digitalprizm.net>
2017-10-23 16:53:31 +0530

    chage email id enquiry@hafary.com.sg

--- delivery_management/api/utility.py
+++ delivery_management/api/utility.py
@@ -70,7 +70,7 @@ def send_delivery_dispatch_alert(name):
 	subject = _("Your Hafary order is delivered")
 	
 	# sender = frappe.session.user not in STANDARD_USERS and frappe.session.user or None
-	sender = "contact@digitalprizm.net"
+	sender = "enquiry@hafary.com.sg"
 	if ds_doc.is_return=="No":
 		message="Hi "+ds_doc.contact_person_name+","+" <br> Your Delivery with DN:"+ds_doc.delivery_note_no +" is delivered.<br>For more info click here   "+url_link+"<br>"+"Kindly Find the attachment."
 	elif ds_doc.is_return=="Yes":
