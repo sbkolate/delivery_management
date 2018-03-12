76b88c5bee863b995cda2075de643656f109aabb
sambhaji <kolate.sambhaji@gmail.com>
2018-03-12 13:10:06 +0530

    fixed report

--- delivery_management/delivery_management/report/delivery_schedule_tracking/delivery_schedule_tracking.py
+++ delivery_management/delivery_management/report/delivery_schedule_tracking/delivery_schedule_tracking.py
@@ -18,36 +18,34 @@ def execute(filters=None):
 
 
 def  get_colums():
-	columns =["Id:Link/Delivery Schedule:95"]+["Date:Data:95"]+["Driver Id:Link/Driver:120"]+["Driver:data:120"]+["Customer:Link/Customer:120"]+["Address:data:200"]+["Mobile No:Data:120"]+["Contact No:Data:120"]\
-						 +["D/O No:data:80"]+["Trip:Data:60"]+["Lorry No:Link/Carrier"]+["Is Return:data:100"]+["Remark:data:250"]
+	columns =["Id:Link/Delivery Schedule:95"]+["Date:Data:95"]\
+		+["Driver:data:120"]\
+		+["Customer:Link/Customer:120"]+["Address:data:200"]\
+		+["Mobile No:Data:120"]+["Contact No:Data:120"]\
+		+["D/O No:data:80"]+["Trip:Data:60"]+["Driver Id:Link/Driver:120"]\
+		+["Lorry No:Link/Carrier"]\
+		+["Is Return:data:100"]+["Remark:data:250"]
 	return columns
 
 
 def get_data(filters):
 	# date = filters.get("from_date")
 	filter_condition = ""
-	# filter_condition += " where date = '" + filters.get("from_date") + "'"
-	# if filters.get("from_date"):
-	# 	filter_condition += " where date BETWEEN '"+ filters.get("from_date")+"'AND'"+filters.get("to_date")+"'"
 	if filters.get("from_date"):
 		filter_condition += " where date = '" + filters.get("from_date") + "'"
 	
-	if filters.get("from_date") or filters.get("carrier") or filters.get("delivery_note_no"):
-		filter_condition += " and delivery_note_no = '" + filters.get("delivery_note_no") +"'"
-		print("################")
-		print(filter_condition)
 
-	elif filters.get("carrier"):
-		filter_condition += " where carrier = '" + filters.get("carrier") + "'"
+	if filters.get("carrier"):
+		filter_condition += " and carrier = '" + filters.get("carrier") + "'"
 	
-	elif filters.get("delivery_note_no"):
-		filter_condition += " where delivery_note_no = '" + filters.get("delivery_note_no") + "'"
+	if filters.get("delivery_note_no"):
+		filter_condition += "  and delivery_note_no LIKE '%" + filters.get("delivery_note_no") + "%'"
 	
 	dl = frappe.db.sql("""select 
-		name,DATE_FORMAT(date,"%d-%m-%Y"),driver,driver_full_name,
+		name,DATE_FORMAT(date,"%d-%m-%Y"),driver_full_name,
 		customer_ref,
 		CONCAT(address_line_1,' ',address_line_2,' ',address_line_3)AS Address,mobile_no,
-		contact_no,delivery_note_no,trip,carrier,is_return,remarks
+		contact_no,delivery_note_no,trip,driver,carrier,is_return,remarks
 		from `tabDelivery Schedule`
 		{0} 
 		 ORDER BY driver,trip,modified desc""".format(filter_condition),as_list=1,debug=1)
